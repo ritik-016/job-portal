6f2f112bce2d989f71fb7abf6116917908b9d717
Ritik Kumar <ad6360153@gmail.com>
2025-06-24 21:14:16 +0530

    fix: correct applicant field name in application model and enhance job application handling in controller

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -1,4 +1,5 @@
-import { Application } from "../models/application.model";
+import { Application } from "../models/application.model.js";
+import Job from "../models/job.model.js";
 
 
 export const applyJob = async (req, res) => {
@@ -20,7 +21,64 @@ export const applyJob = async (req, res) => {
                 success: false
             });
         }
+
+        //check if the job exists
+        const job = await Job.findById(jobId)
+        if(!job){
+            return res.status(404).json({
+                message: "Job not found",
+                success: false
+            });
+        }
+
+        //create a new application
+        const newApplication = new Application.create({
+            job: jobId,
+            application: userId,
+        })
+
+        job.applicants.push(newApplication._id);
+        await job.save();
+        return res.status(201).json({
+            message: "Application submitted successfully",
+            success: true,
+        });
     } catch (error) {
         console.log(error)
+    }
+};
+
+export const getAppliedJobs = async (req, res) => {
+    try {
+        const userId = req.id;
+        const application = await Application.find({ applicant: userId }).sort({ createdAt: -1 }).populate({ 
+            path: 'job', 
+            options: {sort: { createdAt: -1 }},
+            populate: {
+                path: 'companyId',
+                options: {sort: { createdAt: -1 }},
+            }
+        });
+        if(!application){
+            return res.status(404).json({
+                message: "No applications found",
+                success: false
+            });
+        };
+
+        return res.status(200).json({
+            application,
+            success: true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getApplications = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
     }
 }
--- backend/models/application.model.js
+++ backend/models/application.model.js
@@ -6,7 +6,7 @@ const applicationSchema = new mongoose.Schema({
         ref: 'Job',
         required: true
     },
-    applcation: {
+    applicant: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
